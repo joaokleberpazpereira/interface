7d52749a849d5773ace7c11b66204f7fad6e5a47
joaokleberpazpereira <109004816+joaokleberpazpereira@users.noreply.github.com>
2023-07-28 04:39:38 -0300

    tentativa 2

--- src/arrays/ArrayInicial.java
+++ src/arrays/ArrayInicial.java
@@ -1,6 +1,8 @@
 package arrays;
 
 public class ArrayInicial {
+	// cria e gera saída de arrays bidimensionais;
+	// Estudos do livro: Java - Como Programar (Paul Deitel, Harvey Deitel)
 	public static void main(String[] args) {
 		int[][] array1 = {{1 , 2 ,3}, {4, 5, 6}};// fim do arrey 1
 		int[][] array2 = {{1, 2},{3},{4, 5, 6}};// fim do array 2
@@ -10,7 +12,7 @@ public class ArrayInicial {
 		System.out.printf("%nO valor da segunda matriz, por linhas são:%n");
 		saidaDaMatriz(array2);// Exibe array 2 por linha
 	}// fim do método principal
-	
+	// recebendo um método bidimensional
 	public static void saidaDaMatriz(int[][] array) 
 	{
 		// faz um loop pelas linhas da matriz
